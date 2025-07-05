d1a3b3a6f97a418e4a2d990390ef5d5d88cc8785
sadicangel <sadicangel@hotmail.com>
2025-07-05 23:42:34 +0100

    Use IndentedTextWriter and some more cases

--- src/Reflexor/ProxyGenerator.cs
+++ src/Reflexor/ProxyGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Immutable;
+﻿using System.CodeDom.Compiler;
+using System.Collections.Immutable;
 using System.Runtime.CompilerServices;
 using System.Text;
 using Microsoft.CodeAnalysis;
@@ -9,7 +10,7 @@ namespace Reflexor;
 
 public readonly record struct Proxy(string Name, string? Namespace, string TargetType, ImmutableArray<Property> Properties);
 
-public readonly record struct Property(string Name, string Type);
+public readonly record struct Property(string Name, string Type, bool IsReadOnly);
 
 [Generator]
 public sealed class ProxyGenerator : IIncrementalGenerator
@@ -21,23 +22,30 @@ public sealed class ProxyGenerator : IIncrementalGenerator
                 predicate: (syntax, _) => syntax.IsKind(SyntaxKind.ClassDeclaration) || syntax.IsKind(SyntaxKind.RecordDeclaration),
                 transform: (context, _) =>
                 {
-                    var symbol = Unsafe.As<INamedTypeSymbol>(context.TargetSymbol);
-                    var properties = new HashSet<Property>(PropertyComparer.Instance);
-                    foreach (var propertySymbol in symbol.GetMembers().OfType<IPropertySymbol>())
+                    var targetType = Unsafe.As<INamedTypeSymbol>(context.TargetSymbol);
+                    var properties = new Dictionary<string, Property>();
+                    foreach (var member in targetType.GetMembers())
                     {
-                        var property = new Property(propertySymbol.Name, propertySymbol.Type.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat));
-                        properties.Remove(property);
-                        if (!propertySymbol.IsReadOnly)
+                        switch (member)
                         {
-                            properties.Add(property);
+                            case IPropertySymbol propertySymbol:
+                                properties[propertySymbol.Name] = new Property(
+                                    propertySymbol.Name,
+                                    propertySymbol.Type.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat),
+                                    propertySymbol.IsReadOnly
+                                        || (properties.TryGetValue(propertySymbol.Name, out var existing) && existing.IsReadOnly));
+                                break;
+
+                            case IMethodSymbol methodSymbol:
+                                break;
                         }
                     }
 
                     var proxy = new Proxy(
-                        Name: $"{symbol.Name}Proxy",
-                        Namespace: symbol.ContainingNamespace.IsGlobalNamespace ? null : symbol.ContainingNamespace.ToDisplayString(SymbolDisplayFormat.CSharpErrorMessageFormat),
-                        TargetType: symbol.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat),
-                        Properties: [.. properties]);
+                        Name: $"{targetType.Name}Proxy",
+                        Namespace: targetType.ContainingNamespace.IsGlobalNamespace ? null : targetType.ContainingNamespace.ToDisplayString(SymbolDisplayFormat.CSharpErrorMessageFormat),
+                        TargetType: targetType.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat),
+                        Properties: [.. properties.Values]);
 
                     return proxy;
                 });
@@ -45,57 +53,105 @@ public sealed class ProxyGenerator : IIncrementalGenerator
 
         context.RegisterImplementationSourceOutput(proxyProvider, (context, proxy) =>
         {
-            var property = proxy.Properties[0];
-            context.AddSource($"{proxy.Name}.g.cs", SourceText.From($$""""
-                namespace {{proxy.Namespace}}
-                {
-                    public partial struct {{proxy.Name}}
-                    {
-                        private readonly {{proxy.TargetType}} _target;
-
-                        public {{proxy.Name}}({{proxy.TargetType}} target)
-                        {
-                            _target = target ?? throw new System.ArgumentNullException(nameof(target));
-                        }
-
-                        private void ThrowInvalidOperationIfNotInitialized()
-                        {
-                            if (_target is null)
-                            {
-                                throw new global::System.InvalidOperationException("Proxy for '{{proxy.TargetType[8..]}}' is uninitialized");
-                            }
-                        }
-                        {{string.Join("\n", proxy.Properties.Select(property => $$"""
-
-                                public {{property.Type}} {{property.Name}}
-                                {
-                                    get
-                                    {
-                                        ThrowInvalidOperationIfNotInitialized();
-                                        return _target.{{property.Name}};
-                                    }
-                                    set
-                                    {
-                                        ThrowInvalidOperationIfNotInitialized();
-                                        Set{{property.Name}}(_target, value);
-
-                                        [global::System.Runtime.CompilerServices.UnsafeAccessor(global::System.Runtime.CompilerServices.UnsafeAccessorKind.Method, Name = "set_{{property.Name}}")]
-                                        extern static void Set{{property.Name}}({{proxy.TargetType}} target, {{property.Type}} value);
-                                    }
-                                }
-                        """))}}
-                    }
-                }
-                """",
-                Encoding.UTF8));
+            using var stream = new StringWriter();
+            using var writer = new IndentedTextWriter(stream);
+            writer.WriteProxy(proxy);
+            writer.Flush();
+            context.AddSource($"{proxy.Name}.g.cs", SourceText.From(stream.ToString(), Encoding.UTF8));
         });
     }
+}
 
-    private sealed class PropertyComparer : IEqualityComparer<Property>
+public static class IndentedTextWriterExtensions
+{
+    public static void WriteProxy(this IndentedTextWriter writer, Proxy proxy)
     {
-        public static readonly PropertyComparer Instance = new();
+        if (!string.IsNullOrEmpty(proxy.Namespace))
+        {
+            writer.WriteLine($"namespace {proxy.Namespace}");
+            writer.WriteLine("{");
+            writer.Indent++;
+        }
 
-        public bool Equals(Property x, Property y) => x.Name == y.Name;
-        public int GetHashCode(Property obj) => obj.Name?.GetHashCode() ?? 0;
+        writer.WriteLine($"public partial struct {proxy.Name}");
+        writer.WriteLine("{");
+        writer.Indent++;
+        writer.WriteLine($"private readonly {proxy.TargetType} _target;");
+        writer.WriteLine();
+
+        writer.WriteLine($"public {proxy.Name}({proxy.TargetType} target)");
+        writer.WriteLine("{");
+        writer.Indent++;
+        writer.WriteLine("_target = target ?? throw new System.ArgumentNullException(nameof(target));");
+        writer.Indent--;
+        writer.WriteLine("}");
+        writer.WriteLine();
+
+        writer.WriteLine("private void ThrowInvalidOperationIfNotInitialized()");
+        writer.WriteLine("{");
+        writer.Indent++;
+        writer.WriteLine("if (_target is null)");
+        writer.WriteLine("{");
+        writer.Indent++;
+        writer.WriteLine($"throw new global::System.InvalidOperationException(\"Proxy for '{proxy.TargetType[8..]}' is uninitialized\");");
+        writer.Indent--;
+        writer.WriteLine("}");
+        writer.Indent--;
+        writer.WriteLine("}");
+        writer.WriteLine();
+
+        foreach (var property in proxy.Properties)
+        {
+            writer.WriteProperty(property, proxy);
+            writer.WriteLine();
+        }
+
+        writer.WriteLine($"public static implicit operator {proxy.Name}({proxy.TargetType} target) => new {proxy.Name}(target);");
+        writer.WriteLine();
+
+        writer.Indent--;
+        writer.WriteLine("}");
+
+        if (!string.IsNullOrEmpty(proxy.Namespace))
+        {
+            writer.Indent--;
+            writer.Write("}");
+        }
+    }
+
+    public static void WriteProperty(this IndentedTextWriter writer, Property property, Proxy proxy)
+    {
+        writer.WriteLine($"public {property.Type} {property.Name}");
+        writer.WriteLine("{");
+        writer.Indent++;
+
+        writer.WriteLine("get");
+        writer.WriteLine("{");
+        writer.Indent++;
+        writer.WriteLine("ThrowInvalidOperationIfNotInitialized();");
+        writer.WriteLine($"return Get{property.Name}(_target);");
+        writer.WriteLine();
+        writer.WriteLine($"[global::System.Runtime.CompilerServices.UnsafeAccessor(global::System.Runtime.CompilerServices.UnsafeAccessorKind.Method, Name = \"get_{property.Name}\")]");
+        writer.WriteLine($"extern static {property.Type} Get{property.Name}({proxy.TargetType} target);");
+        writer.Indent--;
+        writer.WriteLine("}");
+        writer.WriteLine();
+
+        if (!property.IsReadOnly)
+        {
+            writer.WriteLine("set");
+            writer.WriteLine("{");
+            writer.Indent++;
+            writer.WriteLine("ThrowInvalidOperationIfNotInitialized();");
+            writer.WriteLine($"Set{property.Name}(_target, value);");
+            writer.WriteLine();
+            writer.WriteLine($"[global::System.Runtime.CompilerServices.UnsafeAccessor(global::System.Runtime.CompilerServices.UnsafeAccessorKind.Method, Name = \"set_{property.Name}\")]");
+            writer.WriteLine($"extern static void Set{property.Name}({proxy.TargetType} target, {property.Type} value);");
+            writer.Indent--;
+            writer.WriteLine("}");
+        }
+
+        writer.Indent--;
+        writer.WriteLine("}");
     }
 }
