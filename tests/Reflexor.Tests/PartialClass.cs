9972f720a3bc4637ac217f7cf8008a5ebf9ededd
sadicangel <sadicangel@hotmail.com>
2025-07-19 22:32:52 +0100

    Add test for partial classes

---
+++ tests/Reflexor.Tests/PartialClass.cs
@@ -0,0 +1,24 @@
+﻿using Reflexor.Tests.Helpers;
+
+namespace Reflexor.Tests;
+
+public sealed class PartialClass
+{
+    [Fact]
+    public Task Verify() => TestHelper.VerifySourceCode("""
+        using Reflexor;
+
+        namespace Test;
+
+        [GenerateProxy]
+        public partial class PartialClass
+        {
+            public int Part1 { get; set; }
+        }
+
+        public partial class PartialClass
+        {
+            public string Part2 { get; set; }
+        }
+        """);
+}
