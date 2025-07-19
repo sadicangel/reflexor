705f64bfb7f00eb94570c8d20df46b209f0930be
sadicangel <sadicangel@hotmail.com>
2025-07-19 22:15:43 +0100

    Add test for abstract and virtual members

---
+++ tests/Reflexor.Tests/VirtualAbstract.cs
@@ -0,0 +1,27 @@
+﻿using Reflexor.Tests.Helpers;
+
+namespace Reflexor.Tests;
+
+public sealed class VirtualAbstract
+{
+    [Fact]
+    public Task Verify() => TestHelper.VerifySourceCode("""
+        using Reflexor;
+
+        namespace Test;
+
+        [GenerateProxy]
+        public abstract class VirtualAbstractBase
+        {
+            protected abstract string AbstractProp { get; set; }
+            protected virtual int VirtualProp { get; set; }
+        }
+        
+        [GenerateProxy]
+        public class VirtualAbstractDerived : VirtualAbstractBase
+        {
+            protected override string AbstractProp { get; set; }
+            protected override int VirtualProp { get; set; }
+        }
+        """);
+}
