c7ac728c32ebdaf544c849f92b31e36884c6a35a
sadicangel <sadicangel@hotmail.com>
2025-07-19 22:36:14 +0100

    Add tests for extern methods

---
+++ tests/Reflexor.Tests/ExternMethod.cs
@@ -0,0 +1,21 @@
+﻿using Reflexor.Tests.Helpers;
+
+namespace Reflexor.Tests;
+
+public sealed class ExternMethod
+{
+    [Fact]
+    public Task Verify() => TestHelper.VerifySourceCode("""
+        using Reflexor;
+        using System.Runtime.InteropServices;
+
+        namespace Test;
+
+        [GenerateProxy]
+        public partial class ExternMethod
+        {
+            [LibraryImport("NativeLibrary")]
+            public extern void NativeCall();
+        }
+        """);
+}
