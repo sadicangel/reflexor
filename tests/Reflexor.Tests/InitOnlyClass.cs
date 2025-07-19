693a904a325d886c71d9e5d1ea2e5dc384e5a848
sadicangel <sadicangel@hotmail.com>
2025-07-19 22:10:37 +0100

    Add test for init only class

---
+++ tests/Reflexor.Tests/InitOnlyClass.cs
@@ -0,0 +1,20 @@
+﻿using Reflexor.Tests.Helpers;
+
+namespace Reflexor.Tests;
+
+public sealed class InitOnlyClass
+{
+    [Fact]
+    public Task Verify() => TestHelper.VerifySourceCode("""
+        using Reflexor;
+
+        namespace Test;
+
+        [GenerateProxy]
+        public class InitOnlyClass
+        {
+            public string Name { get; init; }
+            public int Age { get; init; }
+        }
+        """);
+}
