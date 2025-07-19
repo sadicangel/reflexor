b97496dbf215731b797bf9387a9104fd6dcc1fd1
sadicangel <sadicangel@hotmail.com>
2025-07-19 22:11:53 +0100

    Add test for access modifiers

---
+++ tests/Reflexor.Tests/AccessModifiers.cs
@@ -0,0 +1,22 @@
+﻿using Reflexor.Tests.Helpers;
+
+namespace Reflexor.Tests;
+
+public sealed class AccessModifiers
+{
+    [Fact]
+    public Task Verify() => TestHelper.VerifySourceCode("""
+        using Reflexor;
+
+        namespace Test;
+
+        [GenerateProxy]
+        public class AccessModifiers
+        {
+            public int PublicProp { get; set; }
+            protected int ProtectedProp { get; set; }
+            internal int InternalProp { get; set; }
+            private int PrivateProp { get; set; }
+        }
+        """);
+}
