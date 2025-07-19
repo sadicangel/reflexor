0921592d3ab2ecd7282a80c111258bdcb6ca93c2
sadicangel <sadicangel@hotmail.com>
2025-07-19 23:33:41 +0100

    Add tests for ref, in and out params

---
+++ tests/Reflexor.Tests/RefInOutMethods.cs
@@ -0,0 +1,23 @@
+﻿using Reflexor.Tests.Helpers;
+
+namespace Reflexor.Tests;
+
+public sealed class RefInOutMethods
+{
+    [Fact]
+    public Task Verify() => TestHelper.Verify("""
+        using Reflexor;
+
+        namespace Test;
+
+        [GenerateProxy]
+        public class RefInOutMethods
+        {
+            public void DoWork(ref int x, in int y, out int z)
+            {
+                z = x + y;
+                x++;
+            }
+        }
+        """);
+}
